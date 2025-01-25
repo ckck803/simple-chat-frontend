57b1d56d4b9184a50a73e6b2c4fa66c4d3ae4c2a
Dongwoo Yang <ckck803@gmail.com>
2025-01-26 00:01:39 +0900

    index.vue 초기 버전으로 재 수정

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,19 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
   <!--  <div class="flex min-h-screen bg-gray-100">-->
 
-  <div
-      class="xs:relative md:static h-full flex xs:flex-col md:flex-row overflow-hidden"
-  >
+  <!--  <div-->
+  <!--      class="xs:relative md:static h-full flex xs:flex-col md:flex-row overflow-hidden"-->
+  <!--  >-->
+  <!--    <Sidebar/>-->
+  <!--    <div-->
+  <!--        class="xs:absolute xs:z-10 md:static grow h-full xs:w-full md:w-fit scrollbar-hidden bg-white dark:bg-gray-800 transition-all duration-500">-->
+  <!--      &lt;!&ndash; 오른쪽 패널: 채팅 인터페이스 &ndash;&gt;-->
+  <!--      <ChatRoom/>-->
+  <!--    </div>-->
+  <!--  </div>-->
+  <div class="flex min-h-screen bg-gray-100">
     <Sidebar/>
-    <div
-        class="xs:absolute xs:z-10 md:static grow h-full xs:w-full md:w-fit scrollbar-hidden bg-white dark:bg-gray-800 transition-all duration-500">
-      <!-- 오른쪽 패널: 채팅 인터페이스 -->
-      <ChatRoom/>
-    </div>
-<!--    <ChatRoom/>-->
+    <ChatRoom/>
   </div>
 </template>
 
